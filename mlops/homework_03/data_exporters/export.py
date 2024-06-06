e5cb453b503cf64b0fd8c0e2a29f3eea34390631
Asfand Qazi <ayqazi@gmail.com>
2024-06-06 22:22:21 +0100

    HW3 Q6

--- mlops/homework_03/data_exporters/export.py
+++ mlops/homework_03/data_exporters/export.py
@@ -1,3 +1,8 @@
+import mlflow
+import tempfile
+import pickle
+from pathlib import Path
+
 if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
 
@@ -15,6 +20,21 @@ def export_data(data, *args, **kwargs):
         Optionally return any object and it'll be logged and
         displayed when inspecting the block run.
     """
+    mlflow.set_tracking_uri("http://mlflow:5000")
+    print(f"tracking URI: '{mlflow.get_tracking_uri()}'")
+    mlflow.set_experiment("mlops-zoomcamp-2024-homework-03")
     model, dv = data
+
+    # Why is this needed? Otherwise start_run complains about a run already running
+    mlflow.end_run()
+
+    with mlflow.start_run():
+        mlflow.sklearn.log_model(model, "models")
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            tmpfile = Path(tmp_dir, "dict_vectorizer.dat")
+            with tmpfile.open('wb') as f:
+                pickle.dump(dv, f)
+            mlflow.log_artifact(str(tmpfile))
 
     return model, dv
